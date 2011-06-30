99ab1302e6a21ba963ba0742fae0c44e0d1bc735
vaqeteart <vaqeteart@5a8c56f8-3b12-4442-85fe-66c00c5eb458>
2011-06-30 01:04:12 +0000

    添加了两个错误的数组初始化和赋值方式

--- codes/c_cppDemo/02_initArrayTest/main.cpp
+++ codes/c_cppDemo/02_initArrayTest/main.cpp
@@ -9,11 +9,11 @@ using std::endl;
 int main(int argc, char *argv[])
 {
 	int a[10] = {10};
-	int b[10] = {10,};
+	int b[10] = {10,9,};
 	int c[10];
 
 	cout<<"int a[10]={10};"<<endl;
-	cout<<"int b[10]={10,};"<<endl;
+	cout<<"int b[10]={10,9,};"<<endl;
 	cout<<"int c[10];"<<endl;
 	cout<<"the initialize value of a,b,c is:"<<endl;
 	for(int i = 0; i < 10; ++i)
@@ -22,5 +22,8 @@ int main(int argc, char *argv[])
 		cout<<"b["<<i<<"]="<<b[i]<<endl;
 		cout<<"c["<<i<<"]="<<c[i]<<endl;
 	}
+	//c = {1,2,};//这样的赋值是错误的，只能初始化的时候才能这样赋值
+	//c = a;//这样的赋值是错误的
+	//int d[10] = b;//这样的初始化也是错误的
 	return 0;
 }
