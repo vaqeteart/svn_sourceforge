63075b7650a52f3e6f7f9e2ebfb8c236a1043bad
vaqeteart <vaqeteart@5a8c56f8-3b12-4442-85fe-66c00c5eb458>
2011-09-05 07:50:14 +0000

    添加了测试big_little endian的一个方法

--- trunk/codes/c_cppDemo/00_miscellaneous/testBigLittleEndian/main.c
+++ trunk/codes/c_cppDemo/00_miscellaneous/testBigLittleEndian/main.c
@@ -1,5 +1,18 @@
 //判断是little endian还是big endian
+//这里给出两个方法，第二个方法使用了联合，封装在一个函数中进行实现。
 #include <stdio.h>
+int is_big_endian(void)
+{
+	printf("judge by %s:\n",__FUNCTION__);
+	union
+	{
+		long l;
+		char bytes[sizeof(long)];
+	} u;
+	u.l = 1;
+	return (u.bytes[sizeof(long)-1]==1);
+}
+
 int main(int argc, char *argv[])
 {
 	int a = 0x11223344;
@@ -16,5 +29,14 @@ int main(int argc, char *argv[])
 	}
 	//强转之后的地址，一定是从低开始算。
 	printf("%x\n",(int)(char)b);
+
+	if(is_big_endian())
+	{
+		printf("is big endian. \n");
+	}
+	else
+	{
+		printf("is little endian. \n");
+	}
 	printf("hello\n");
 }
