fb98e9ab4fe2c55199998640719b62e9b917e405
vaqeteart <vaqeteart@5a8c56f8-3b12-4442-85fe-66c00c5eb458>
2012-07-05 14:51:15 +0000

    run command without intervetion

--- codes/shellDemo/00_miscellaneous/file_gener.sh
+++ codes/shellDemo/00_miscellaneous/file_gener.sh
@@ -4,3 +4,17 @@ This is the content of the file.
 you can input any thing, with 
 but \$ with \\
 filecontent
+
+
+#The command below run fdisk without shell intervention.
+#fdisk "/dev/sdb1" << END
+#n
+#p
+#1
+#1
+#+64M
+#t
+#e
+#p
+#w
+#END
