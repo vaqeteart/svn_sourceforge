166ff3e7bbb6d4c1ba3309ad1a3e8a16f32d8d4e
vaqeteart <vaqeteart@5a8c56f8-3b12-4442-85fe-66c00c5eb458>
2012-11-21 08:49:07 +0000

    add function var arg modify

--- codes/pythonDemo/01_funvararg.py
+++ codes/pythonDemo/01_funvararg.py
@@ -5,7 +5,13 @@ def fun(arg1, arg2=None):
     print arg2
 
 def mydebug(*args, **kargs):
+    print "-"*10
     fun(args[0],kargs.get("arg2",None))
+    print "-"*10
+    fun(args[0],**kargs)
+    print "-"*10
+    fun(*args,**kargs)
+    print "-"*10
 
 mydebug("arg1","arg2")
 mydebug("arg1",arg2 = "arg2")
