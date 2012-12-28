4b80070cb11eb7486efd2402639ab0d79da4f69c
vaqeteart <vaqeteart@5a8c56f8-3b12-4442-85fe-66c00c5eb458>
2012-12-28 10:06:24 +0000

    add regex search

---
+++ codes/pythonDemo/13_regex_search.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import re
+myStr = "helloworld"
+myReg = "^.*lo"
+
+match = re.search(myReg, myStr)
+print match.group(0)
+
+myHtmlPage = """
+<html>
+<head>
+</head>
+<body>
+<form>
+<input name="aa" value="11">
+<input name="bb" value="22">
+</form>
+</body>
+</html>
+"""
+myReg = r"<form.*/form>"
+match = re.search(myReg, myHtmlPage, re.S)
+formContent = match.group(0)
+print formContent
+
+selfMatch = re.compile(r"\s*?<input .*? name.*?>", re.S)
+print selfMatch.findall(formContent)
+
